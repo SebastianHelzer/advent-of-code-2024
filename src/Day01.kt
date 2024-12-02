ad73c2a4645ffd334eabc1ff1b8bcff41a8791ad
Sebastian Helzer <sebastian@helzer.eu>
2024-12-02 14:40:42 -0700

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,34 @@
+import kotlin.math.abs
+
 fun main() {
+    fun numbersLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val firsts = mutableListOf<Int>()
+        val seconds = mutableListOf<Int>()
+        input.forEach {
+            val (first, second) = it.split(regex = Regex("\\s+"))
+            firsts.add(first.toInt())
+            seconds.add(second.toInt())
+        }
+        return Pair(firsts, seconds)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (firsts, seconds) = numbersLists(input)
+        val sortedSeconds = seconds.sorted()
+        return firsts.sorted().mapIndexed { index, i -> abs(sortedSeconds[index] - i)  }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (firsts, seconds) = numbersLists(input)
+        return firsts.sumOf { value ->
+            seconds.filter { it == value }.sum()
+        }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
